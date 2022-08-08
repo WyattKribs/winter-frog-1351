6afa72143b56f2ced3a258de35d5af45b15d56a6
Wyatt Kribs <kribswyatt@gmail.com>
2022-08-08 12:00:05 -0600

    finished story 1, working on story 2

--- spec/features/plot/index_spec.rb
+++ spec/features/plot/index_spec.rb
@@ -10,13 +10,26 @@ describe "plot index" do
     plant2 = Plant.create!(name: "Snap Peas", description: "Crunchy", days_to_harvest: 10, plots_id: plot2.id)
     plant3 = Plant.create!(name: "Kale", description: "Iron rich", days_to_harvest: 10, plots_id: plot1.id)
     plant4 = Plant.create!(name: "Spinach", description: "Smells nice", days_to_harvest: 10, plots_id: nil)
-    # plot_plant1 = PlotPlant.create!(plot_id: plot1.id, plant_id: plant1.id)
-    # plot_plant2 = PlotPlant.create!(plot_id: plot1.id, plant_id: plant2.id)
 
     visit '/plots'
-    save_and_open_page
+
     expect(page).to have_content("Plot ##{plot1.number}")
     expect(page).to have_content("#{plant1.name}")
     expect(page).to_not have_content("#{plant4.name}")
+  end
+
+  it "displays each plot and the plants in the plot" do
+    garden1 = Garden.create!(name: "Osage Gardens", organic: true)
+    plot1 = Plot.create!(number: 1, size: "Medium", direction: "West", garden_id: garden1.id)
+    plot2 = Plot.create!(number: 2, size: "Large", direction: "North", garden_id: garden1.id)
+    plot3 = Plot.create!(number: 3, size: "Large", direction: "North", garden_id: garden1.id)
+    plant1 = Plant.create!(name: "Sage", description: "Smells nice", days_to_harvest: 10, plots_id: plot1.id)
+    plant2 = Plant.create!(name: "Snap Peas", description: "Crunchy", days_to_harvest: 10, plots_id: plot2.id)
+    plant3 = Plant.create!(name: "Kale", description: "Iron rich", days_to_harvest: 10, plots_id: plot1.id)
+    plant4 = Plant.create!(name: "Spinach", description: "Smells nice", days_to_harvest: 10, plots_id: nil)
+
+    visit '/plots'
+    save_and_open_page
+    expect(page).to have_link("REMOVE")
   end
 end
